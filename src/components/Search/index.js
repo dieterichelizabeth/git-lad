3910b6a69c11800d2cd82ca0f2d4fc5eb0c80328
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-04 04:18:36 -0500

    Adds content to search results container

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,52 +1,69 @@
 import React from "react";
+import SearchResults from "../SearchResults";
 
 function SearchPage() {
+  const userSearch = (
+    <div className="search-type-div">
+      <p className="search-heading">Search for Repositories by User</p>
+      <form id="user-form">
+        <input
+          name="username"
+          id="username"
+          type="text"
+          autoFocus={true}
+          placeholder="Ex. Octocat"
+          className="form-input"
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+
+  const languageSearch = (
+    <div className="search-type-div">
+      <p className="search-heading">Search for Repositories by Topic</p>
+      <div className="language-buttons">
+        <button className="lng-btn">HTML</button>
+        <button className="lng-btn">CSS</button>
+        <button className="lng-btn">JavaScript</button>
+        <button className="lng-btn">React.js</button>
+        <button className="lng-btn">MongoDb</button>
+        <button className="lng-btn">Python</button>
+      </div>
+    </div>
+  );
+
+  const repoSearch = (
+    <div className="search-type-div">
+      <p className="search-heading">Search for Repositories by Name</p>
+      <form id="repo-form">
+        <input
+          name="reponame"
+          id="reponame"
+          type="text"
+          autoFocus={true}
+          placeholder="Ex. blueberry-pie"
+          className="form-input"
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+
   return (
-    <div>
+    <div className="flex-display">
       <div className="search-container">
-        <div className="search-type-div">
-          <p className="search-heading">Search for Repositories by User</p>
-          <form id="user-form">
-            <input
-              name="username"
-              id="username"
-              type="text"
-              autofocus="true"
-              placeholder="Ex. Octocat"
-              className="form-input"
-            />
-            <button type="submit" class="search-btn">
-              Search
-            </button>
-          </form>
-        </div>
-        <div className="search-type-div">
-          <p className="search-heading">Search for Repositories by Topic</p>
-          <div className="language-buttons">
-            <button className="lng-btn">HTML</button>
-            <button className="lng-btn">CSS</button>
-            <button className="lng-btn">JavaScript</button>
-            <button className="lng-btn">React.js</button>
-            <button className="lng-btn">MongoDb</button>
-            <button className="lng-btn">Python</button>
-          </div>
-        </div>
-        <div className="search-type-div">
-          <p className="search-heading">Search for Repositories by Name</p>
-          <form id="repo-form">
-            <input
-              name="reponame"
-              id="reponame"
-              type="text"
-              autofocus="true"
-              placeholder="Ex. blueberry-pie"
-              className="form-input"
-            />
-            <button type="submit" class="search-btn">
-              Search
-            </button>
-          </form>
-        </div>
+        {userSearch}
+        {languageSearch}
+        {repoSearch}
+      </div>
+      <div className="search-results">
+        <p className="result-heading">Results for:</p>
+        <SearchResults />
       </div>
     </div>
   );
---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SearchResults() {
+  return (
+    <div className="result-info">
+      <p>
+        Repositories will populate here, you can view the open issues count.
+        Click on the repo name to see more information.
+      </p>
+
+      <p>
+        Click on one of the languages to the left to get started! Or enter a
+        username in the username search bar or repository name in the repo
+        search bar.{" "}
+      </p>
+
+      <p>
+        Repositories will populate here, you can view the open issues count.
+        Click on the repo name to see more information and to go the repository
+        itself on github!
+      </p>
+    </div>
+  );
+}
+
+export default SearchResults;
