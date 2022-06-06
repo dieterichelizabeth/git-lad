8b7c9f80512bc3df87f038ad3663b32179f58e1a
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-05 22:10:15 -0500

    Displays Repo information for Issues page

--- src/pages/SearchIssues.js
+++ src/pages/SearchIssues.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import SearchResults from "../components/SearchResults";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import IssueCards from "../components/IssueCards";
 
 function SearchIssues() {
-  //   let searchTerms = window.location.href.split("/");
-  //   let repoName = `${searchTerms[5]}/${searchTerms[6]}`;
+  const [repoInfo, setRepoInfo] = useState("");
+  const [repoIssues, setRepoIssues] = useState("");
+
+  let searchTerms = window.location.href.split("/");
+  let repoName = `${searchTerms[5]}/${searchTerms[6]}`;
+
+  useEffect(() => {
+    handleRepoSearch(repoName);
+  }, []);
 
   //   let apiUrl =
   //     "https://api.github.com/repos/" + repoName + "/issues?direction=asc";
@@ -26,27 +33,68 @@ function SearchIssues() {
   //       alert("Unable to connect to GitHub");
   //     });
 
+  const handleRepoSearch = async (repoName) => {
+    let apiUrl = "https://api.github.com/repos/" + repoName;
+
+    fetch(apiUrl)
+      .then(function (res) {
+        if (res.ok) {
+          res.json().then((json) => {
+            console.log(json);
+            setRepoInfo(json);
+          });
+        } else {
+          alert("Error: GitHub User Not Found");
+        }
+      })
+
+      .catch(function (error) {
+        alert("Unable to connect to GitHub");
+      });
+  };
+
   return (
     <>
       <Nav />
-      <button className="back-button">Back to Search</button>
-      <div className="issues-info-container">
-        <h1 className="issues-heading">
-          Now Showing Issues For:{" "}
-          <span className="issues-repository-name">JFlo1981/Virtual-Zoo</span>
-        </h1>
-        <p className="issues-description">
-          A weather dashboard where users can search by cityname for a 5 day
-          forecast
-        </p>
-        <p className="issues-dates">Created at: 2022-05-09</p>
-        <p className="issues-dates">Published at: 2022-05-09</p>
-      </div>
+      <button href={"/search"} className="back-button">
+        Back to Search
+      </button>
 
-      <p className="issue-number">832 Issues</p>
-      <hr></hr>
+      {repoInfo ? (
+        <>
+          <div className="issues-info-container">
+            <h1 className="issues-heading">
+              Now Showing Issues For:{" "}
+              <span>
+                <a
+                  href={repoInfo.svn_url}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="issues-repository-name"
+                >
+                  {repoInfo.full_name}
+                </a>
+              </span>
+            </h1>
+            <p className="issues-description">{repoInfo.description}</p>
+            <p className="issues-dates">Created at: {repoInfo.created_at}</p>
+            {repoInfo.created_at ? (
+              <p className="issues-dates">
+                Published at: {repoInfo.created_at}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
 
-      <IssueCards />
+          <p className="issue-number">{repoInfo.open_issues} Issue(s)</p>
+          <hr></hr>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+      {repoIssues ? <IssueCards issues={repoIssues} /> : <p>Loading...</p>}
 
       <Footer />
     </>
